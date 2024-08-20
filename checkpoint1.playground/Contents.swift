c4f87ae323d3da22c1ce7d3d7b49a15affa28e8c
Matt Riley <matthew.riley@barren.kyschools.us>
2024-08-20 08:25:59 -0500

    Initial Commit

---
+++ checkpoint1.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Cocoa
+
+let celsius = 25.0
+
+let multiply = celsius * 9
+
+let divide = multiply / 5
+
+let add = divide + 32
+
+print(add)
+
+let cel = 25.0
+let far = cel * 9 / 5 + 32
+print(far)
